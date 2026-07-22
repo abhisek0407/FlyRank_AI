96ea503b1729de4095651506dea59ff01d934efe
04abhisek2006 <abhisekpanda366@gmail.com>
2026-07-22 21:40:09 +0530

    Stage 2: read endpoints with 404

--- week-2_Assignment-1/main.py
+++ week-2_Assignment-1/main.py
@@ -1,5 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel,Field
 app=FastAPI()
+class taskFormat(BaseModel):
+    id:int=Field(description="Unique identifier assigned to each task")
+    title:str=Field(description="Name of the task")
+    done:bool=Field(description="Whether task is completed or not")
+
+
+tasks=[
+    {
+        "id":1,
+        "title":"Office work",
+        "done":True
+    },
+    {
+        "id":2,
+        "title":"Mumbai tour",
+        "done":True
+    },
+    {
+        "id":3,
+        "title":"Marketing",
+        "done":False
+    }
+]
 @app.get("/")
 def root():
     return {'name':'Task API','Version':'1.0','endpoints':["/tasks"]}
@@ -8,3 +32,19 @@ def health():
     return {
         "status":"OK"
     }
+
+@app.get("/tasks")
+def view():
+    return tasks
+
+@app.get("/tasks/{id}")
+def get_task(id:int):
+    for task in tasks:
+        if task["id"]==id:
+            return task
+    return HTTPException(
+        status_code=404,
+        detail=f"Task {id} not found"
+    )
+        
+    
